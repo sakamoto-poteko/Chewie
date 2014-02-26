71787411f841977e716161e9e867b33740749906
Afa.L Cheng <alpha@tomatoeskit.org>
2014-02-26 15:47:16 -0500

    Round Window added

--- include/rectwindow.h
+++ include/rectwindow.h
@@ -1,6 +1,14 @@
 #ifndef RECTWINDOW_H
 #define RECTWINDOW_H
 
+#include <QVector>
+
+struct RoundWindowDef {
+    float xResidual;
+    float yResidual;
+    float radius;
+};
+
 /**
  * @brief The RectWindow class
  *
@@ -15,12 +23,16 @@ public:
 
     bool checkCustomWindow(float xRes, float yRes);
     bool windowEnabled() {return rectWindowEnabled;}
+
 private:
     bool rectWindowEnabled  ;
     float windowLeftResidual;
     float windowTopResidual ;
     float windowRightResidual   ;
     float windowBottomResidual  ;
+    QVector<RoundWindowDef> roundWindows;
+
+    bool pointInCircle(float point_x, float point_y, float circle_x, float circle_y, float circle_radius);
 };
 
 #endif // RECTWINDOW_H
--- src/rectwindow.cpp
+++ src/rectwindow.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <QSettings>
 
 #include "rectwindow.h"
@@ -12,6 +13,22 @@ CustomPixelWindow::CustomPixelWindow()
     windowLeftResidual  = windowConfig.value("LeftX",       0).toFloat();
     windowBottomResidual= windowConfig.value("BottomY",     0).toFloat();
     windowTopResidual   = windowConfig.value("TopY",        0).toFloat();
+
+    for (int i = 0; ;++i) {
+        if (windowConfig.contains(QString("RoundWindow%1_x").arg(i))
+                && windowConfig.contains(QString("RoundWindow%1_y").arg(i))
+                && windowConfig.contains(QString("RoundWindow%1_radius").arg(i))) {
+            RoundWindowDef window;
+            window.xResidual = windowConfig.value(QString("RoundWindow%1_x").arg(i)).toFloat();
+            window.yResidual = windowConfig.value(QString("RoundWindow%1_y").arg(i)).toFloat();
+            window.radius = windowConfig.value(QString("RoundWindow%1_radius").arg(i)).toFloat();
+            roundWindows.append(window);
+        } else {
+            break; // Leave for loop
+        }
+    }
+
+
     windowConfig.endGroup();
 
     if (rectWindowEnabled) {
@@ -20,6 +37,12 @@ CustomPixelWindow::CustomPixelWindow()
                 .arg(windowRightResidual).arg(windowLeftResidual).toStdString(), ACRed);
         STDLINE(QString("\tBottom Y: %1, Top Y: %2")
                 .arg(windowBottomResidual).arg(windowTopResidual).toStdString(), ACRed);
+
+        for (int i = 0; i < roundWindows.size(); ++i) {
+            STDLINE(QString("\tRound Window %1: X: %2, Y: %3, Radius: %4")
+                    .arg(i).arg(roundWindows.at(i).xResidual).arg(roundWindows.at(i).yResidual)
+                    .arg(roundWindows.at(i).radius), ACRed);
+        }
     }
 
 }
@@ -29,9 +52,25 @@ bool CustomPixelWindow::checkCustomWindow(float xRes, float yRes)
     if (!rectWindowEnabled)
         return false;
 
-    if (xRes <= windowRightResidual && xRes >= windowLeftResidual
-            && yRes <= windowTopResidual && yRes >= windowBottomResidual)
-        return true;
-    else
+    if (xRes > windowRightResidual || xRes < windowLeftResidual)
         return false;
+    if (yRes > windowTopResidual || yRes < windowBottomResidual)
+        return false;
+
+    foreach (RoundWindowDef window, roundWindows) {
+        if (!pointInCircle(xRes, yRes, window.xResidual, window.yResidual, window.radius))
+            return false;
+    }
+
+
+    return true;
+
+}
+
+inline bool CustomPixelWindow::pointInCircle(float point_x, float point_y, float circle_x, float circle_y, float circle_radius)
+{
+
+    float delta_x = circle_x - point_x;
+    float delta_y = circle_y - point_y;
+    return (float)sqrt(delta_x*delta_x+delta_y*delta_y) <= circle_radius;
 }
